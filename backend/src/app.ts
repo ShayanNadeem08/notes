1433726500d6e9ed6e1f2a7e0b9f755c912e8258
ShayanNadeem <1shayannadeem@gmail.com>
2025-02-09 13:44:39 +0500

    created new node.ts file to make sure notes show title, text and timestamp

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import Notemodel from "./models/note"
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+app.get("/", async (req, res) => {
+  const notes = await Notemodel.find().exec();
+  res.status(200).json(notes);
 });
 
 export default app;
